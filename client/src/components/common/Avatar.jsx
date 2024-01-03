ecbd391a30ee13d01802d32f3d8e1a77b685dc0f
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-03 12:52:44 +0530

    created contextmenu component in client side

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { FaCamera } from "react-icons/fa";
+import ContextMenu from "./ContextMenu";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setisContextMenuVisible] = useState(false);
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const contextMenuOptions = [
+    {name:"Take Photo", callback:()=>{}},
+    {name:"Choose From Library", callback:()=>{}},
+    {name:"Upload Photo", callback:()=>{}},
+    {name:"Remove Photo", callback:()=>{}},
+  ]
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCoordinates({ x: e.pageX, y: e.pageY });
+    setisContextMenuVisible(true);
+  };
 
   return (
     <>
@@ -28,9 +47,17 @@ function Avatar({ type, image, setImage }) {
               className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
               ${hover ? "visible" : "hidden"}
           `}
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
             >
-              <FaCamera className="text-2xl" id="context-opener" />
-              <span>Change <br/> Profile Photo</span>
+              <FaCamera
+                className="text-2xl"
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              />
+              <span onClick={(e) => showContextMenu(e)} id="context-opener">
+                Change <br /> Profile Photo
+              </span>
             </div>
             <div className="flex items-center justify-center h-60 w-60">
               <Image src={image} alt="avatar" className="rounded-full" fill />
@@ -38,6 +65,14 @@ function Avatar({ type, image, setImage }) {
           </div>
         )}
       </div>
+      {isContextMenuVisible && (
+        <ContextMenu
+          options={contextMenuOptions}
+          coordinates={contextMenuCoordinates}
+          contextMenu={isContextMenuVisible}
+          setContextMenu={setisContextMenuVisible}
+        />
+      )}
     </>
   );
 }
